efd7ef3fb48e443fd95f612659927add9ad34bb5
thumster <bryan.thum@hotmail.com>
2020-09-14 01:09:07 +0800

    UI for login and changing theme based on user

--- HoMED/HoMED-war/src/java/jsf/managedBean/EmployeeLoginManagedBean.java
+++ HoMED/HoMED-war/src/java/jsf/managedBean/EmployeeLoginManagedBean.java
@@ -6,11 +6,15 @@ import javax.enterprise.context.RequestScoped;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
 import javax.inject.Inject;
+import javax.servlet.http.HttpSession;
 import util.helper.ThemeCustomiser;
 
 @Named(value = "employeeLoginManagedBean")
 @RequestScoped
 public class EmployeeLoginManagedBean {
+
+    private String username;
+    private String password;
 
     @Inject
     private ThemeCustomiser themeCustomiser;
@@ -20,12 +24,50 @@ public class EmployeeLoginManagedBean {
 
     // Method to test dynamic theming, not official login method.
     public void login(ActionEvent event) throws IOException {
-        System.out.println("Testing.....2");
 
-        // To dynamically change theme colours next time.
-        themeCustomiser.setComponentTheme("bluegrey");
+        // IF ADMIN ==> use this theme
+        themeCustomiser.setComponentTheme("blbluegreyue");
         themeCustomiser.setTopbarColor("bluegrey");
-        System.out.println("Testing.....");
+
+        // IF DOCTOR ==> use this theme
+//        themeCustomiser.setComponentTheme("blue");
+//        themeCustomiser.setTopbarColor("blue");
+        // Login code here
+//        try {
+//            Employee currentEmployee = employeeSessionBeanLocal.employeeLogin(username, password);
+//
+//            FacesContext.getCurrentInstance().getExternalContext().getSession(true);
+//            FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("currentEmployee", currentEmployee);
+//            FacesContext.getCurrentInstance().getExternalContext().redirect("homepage.xhtml");
+//        } catch (EmployeeInvalidLoginCredentialException ex) {
+//            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Invalid login credential: " + ex.getMessage(), null));
+//        }
         FacesContext.getCurrentInstance().getExternalContext().redirect("homepage.xhtml");
+    }
+
+    public void logout(ActionEvent event) throws IOException {
+        themeCustomiser.setComponentTheme("cyan");
+        themeCustomiser.setTopbarColor("cyan");
+
+        // Invalidating the session and causes the user to logout
+        // ((HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(true)).invalidate();
+        // Temporary Measure - By right security filter will invoke redirection
+        FacesContext.getCurrentInstance().getExternalContext().redirect("login.xhtml");
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
