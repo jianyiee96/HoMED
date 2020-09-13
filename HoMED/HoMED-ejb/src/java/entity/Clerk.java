fafd05c76a0faa0afbc89c59b4f78bb002f7184a
Sunaga Ryuto <e0310069@u.nus.edu>
2020-09-13 20:16:35 +0800

    login halfway

---
+++ HoMED/HoMED-ejb/src/java/entity/Clerk.java
@@ -0,0 +1,48 @@
+/*
+ * Project Title: Home Team Medical Board
+ * Project Application: HoMED-ejb
+ */
+package entity;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author sunag
+ */
+@Entity
+public class Clerk extends Employee implements Serializable {
+
+    
+    public Clerk(){
+    }
+    
+    public Clerk(String name, String nric, String password) {
+        super(name, nric, password);
+    }
+
+
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Clerk)) {
+            return false;
+        }
+        Clerk other = (Clerk) object;
+        if ((super.getEmployeeId() == null && other.getEmployeeId() != null) || (super.getEmployeeId() != null && !super.getEmployeeId().equals(other.getEmployeeId()))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entity.Clerk[ id=" + super.getEmployeeId() + " ]";
+    }
+    
+}
