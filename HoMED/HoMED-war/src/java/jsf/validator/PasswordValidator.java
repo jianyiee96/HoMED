5e4f4c9938b0161520212ede20cafa9f4d775acd
thumster <bryan.thum@hotmail.com>
2020-09-22 22:56:21 +0800

    added validator files and css files

---
+++ HoMED/HoMED-war/src/java/jsf/validator/PasswordValidator.java
@@ -0,0 +1,30 @@
+package jsf.validator;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.FacesValidator;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+
+@FacesValidator("passwordValidator")
+public class PasswordValidator implements Validator {
+
+    public PasswordValidator() {
+    }
+
+    @Override
+    public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
+        String strValue = value.toString();
+
+        if (strValue == null || strValue.equals("")) {
+            throw new ValidatorException(new FacesMessage(FacesMessage.SEVERITY_ERROR, "Validation Error",
+                    "Password is required"));
+        }
+
+        if (strValue.length() < 8 || strValue.length() > 32) {
+            throw new ValidatorException(new FacesMessage(FacesMessage.SEVERITY_ERROR, "Validation Error",
+                    "Password must be between 8 and 32 characters"));
+        }
+    }
+}
