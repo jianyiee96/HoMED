f6ea348a887ed2be07f3f4c8f6036188810f0dec
thumster <bryan.thum@hotmail.com>
2020-09-24 01:21:03 +0800

    Done with employee mgmt touchup

--- HoMED/HoMED-war/src/java/jsf/managedBean/EmployeeAccountManagementManagedBean.java
+++ HoMED/HoMED-war/src/java/jsf/managedBean/EmployeeAccountManagementManagedBean.java
@@ -1,7 +1,3 @@
-/*
- * Project Title: Home Team Medical Board
- * Project Application: HoMED-war
- */
 package jsf.managedBean;
 
 import ejb.session.stateless.EmployeeSessionBeanLocal;
@@ -10,15 +6,12 @@ import java.io.Serializable;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
-import javax.faces.application.FacesMessage;
-import javax.faces.context.FacesContext;
 import javax.inject.Named;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import org.primefaces.PrimeFaces;
 import util.enumeration.EmployeeRoleEnum;
 import util.enumeration.GenderEnum;
-import util.exceptions.CreateEmployeeException;
 
 @Named(value = "employeeAccountManagementManagedBean")
 @ViewScoped
@@ -32,44 +25,17 @@ public class EmployeeAccountManagementManagedBean implements Serializable {
 
     private List<Employee> employees;
 
-    private Employee employeeToCreate;
-    private Boolean isEditableCreateEmployee;
-
     public EmployeeAccountManagementManagedBean() {
     }
 
     @PostConstruct
     public void postConstruct() {
-
         employees = employeeSessionBeanLocal.retrieveAllEmployees();
-        this.employeeToCreate = new Employee();
     }
 
     public void dialogActionListener() {
-
         this.employees = employeeSessionBeanLocal.retrieveAllEmployees();
         PrimeFaces.current().ajax().update("formAllEmployees:dataTableEmployees");
-    }
-
-    public void doViewEmployee(Employee employee) {
-        this.manageEmployeeManagedBean.setEmployeeToView(employee);
-        this.manageEmployeeManagedBean.init();
-    }
-
-    public void doCreateEmployee() {
-
-        this.isEditableCreateEmployee = true;
-    }
-
-    public void createEmployee() {
-        try {
-            employeeSessionBeanLocal.createEmployee(employeeToCreate);
-            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Successfully created Employee! Please inform employee that OTP has been sent to their email.", null));
-            this.isEditableCreateEmployee = false;
-            this.employees = employeeSessionBeanLocal.retrieveAllEmployees();
-        } catch (CreateEmployeeException ex) {
-            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, ex.getMessage(), null));
-        }
     }
 
     public List<Employee> getEmployees() {
@@ -88,28 +54,12 @@ public class EmployeeAccountManagementManagedBean implements Serializable {
         this.manageEmployeeManagedBean = manageEmployeeManagedBean;
     }
 
-    public Employee getEmployeeToCreate() {
-        return employeeToCreate;
-    }
-
-    public void setEmployeeToCreate(Employee employeeToCreate) {
-        this.employeeToCreate = employeeToCreate;
-    }
-
     public GenderEnum[] getGenders() {
         return GenderEnum.values();
     }
 
     public EmployeeRoleEnum[] getEmployeeRoles() {
         return EmployeeRoleEnum.values();
-    }
-
-    public Boolean getIsEditableCreateEmployee() {
-        return isEditableCreateEmployee;
-    }
-
-    public void setIsEditableCreateEmployee(Boolean isEditableCreateEmployee) {
-        this.isEditableCreateEmployee = isEditableCreateEmployee;
     }
 
 }
