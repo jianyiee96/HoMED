d2ddad8a2900a5214bf678f21cdeba7fdc60e598
weekeat-tan <tanwk_97@hotmail.com>
2020-09-25 17:46:37 +0800

    Touched up medical centre view

--- HoMED/HoMED-war/src/java/jsf/managedBean/MedicalCentreManagementManagedBean.java
+++ HoMED/HoMED-war/src/java/jsf/managedBean/MedicalCentreManagementManagedBean.java
@@ -10,15 +10,9 @@ import java.io.Serializable;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
-import javax.faces.application.FacesMessage;
-import javax.faces.context.FacesContext;
 import javax.inject.Named;
 import javax.faces.view.ViewScoped;
 import org.primefaces.PrimeFaces;
-import util.exceptions.CreateMedicalCentreException;
-import util.exceptions.DeleteMedicalCentreException;
-import util.exceptions.MedicalCentreNotFoundException;
-import util.exceptions.UpdateMedicalCentreException;
 
 @Named(value = "medicalCentreManagementManagedBean")
 @ViewScoped
@@ -29,78 +23,17 @@ public class MedicalCentreManagementManagedBean implements Serializable {
 
     private List<MedicalCentre> medicalCentres;
 
-    private MedicalCentre medicalCentreToCreate;
-    private MedicalCentre medicalCentreToManage;
-    private MedicalCentre medicalCentreToDelete;
-
-    private Boolean isEditable;
-
     public MedicalCentreManagementManagedBean() {
     }
 
     @PostConstruct
     public void postConstruct() {
         medicalCentres = medicalCentreSessionBeanLocal.retrieveAllMedicalCentres();
-        medicalCentreToCreate = new MedicalCentre();
     }
 
-    public void create() {
-        try {
-            Long medicalCentreId = medicalCentreSessionBeanLocal.createNewMedicalCentre(medicalCentreToCreate);
-
-            medicalCentres.add(medicalCentreToCreate);
-
-            FacesContext.getCurrentInstance().addMessage("growl-message", new FacesMessage(FacesMessage.SEVERITY_INFO, "Medical Centre", "New medical centre [ID: " + medicalCentreId + "] is created!"));
-            PrimeFaces.current().executeScript("PF('dialogCreateNewMedicalCentre').hide()");
-
-            medicalCentreToCreate = new MedicalCentre();
-        } catch (CreateMedicalCentreException ex) {
-            FacesContext.getCurrentInstance().addMessage("growl-message", new FacesMessage(FacesMessage.SEVERITY_ERROR, ex.getMessage(), null));
-        }
-    }
-
-    public void view(MedicalCentre medicalCentre) {
-        try {
-            // this.medicalCentreToManage = medicalCentre
-            this.medicalCentreToManage = medicalCentreSessionBeanLocal.retrieveMedicalCentreById(medicalCentre.getMedicalCentreId()); // Should not require this if validation does not go through on the front end
-            isEditable = false;
-        } catch (MedicalCentreNotFoundException ex) {
-            FacesContext.getCurrentInstance().addMessage("growl-message", new FacesMessage(FacesMessage.SEVERITY_ERROR, ex.getMessage(), null));
-        }
-    }
-
-    public void edit() {
-        isEditable = true;
-    }
-
-    public void saveEdit() {
-        try {
-            medicalCentreSessionBeanLocal.updateMedicalCentre(medicalCentreToManage);
-            isEditable = false;
-
-            FacesContext.getCurrentInstance().addMessage("growl-message", new FacesMessage(FacesMessage.SEVERITY_INFO, "Medical Centre", "Medical centre [ID: " + medicalCentreToManage.getMedicalCentreId() + "] is updated!"));
-            PrimeFaces.current().executeScript("PF('dialogManageMedicalCentre').hide()");
-
-            medicalCentreToManage = new MedicalCentre();
-            medicalCentres = medicalCentreSessionBeanLocal.retrieveAllMedicalCentres();
-        } catch (UpdateMedicalCentreException ex) {
-            FacesContext.getCurrentInstance().addMessage("growl-message", new FacesMessage(FacesMessage.SEVERITY_ERROR, ex.getMessage(), null));
-        }
-
-    }
-
-    // Need to check if there is any association with consultations/employees in the future
-    public void delete() {
-        try {
-            medicalCentreSessionBeanLocal.deleteMedicalCentre(medicalCentreToDelete.getMedicalCentreId());
-            medicalCentres.remove(medicalCentreToDelete);
-
-            FacesContext.getCurrentInstance().addMessage("growl-message", new FacesMessage(FacesMessage.SEVERITY_INFO, "Medical Centre", "Medical centre [ID: " + medicalCentreToDelete.getMedicalCentreId() + "] is deleted!"));
-
-            medicalCentreToDelete = new MedicalCentre();
-        } catch (DeleteMedicalCentreException ex) {
-            FacesContext.getCurrentInstance().addMessage("growl-message", new FacesMessage(FacesMessage.SEVERITY_ERROR, ex.getMessage(), null));
-        }
+    public void dialogActionListener() {
+        medicalCentres = medicalCentreSessionBeanLocal.retrieveAllMedicalCentres();
+        PrimeFaces.current().ajax().update("formAllMedicalCentres:dataTableMedicalCentres");
     }
 
     public List<MedicalCentre> getMedicalCentres() {
@@ -110,37 +43,4 @@ public class MedicalCentreManagementManagedBean implements Serializable {
     public void setMedicalCentres(List<MedicalCentre> medicalCentres) {
         this.medicalCentres = medicalCentres;
     }
-
-    public MedicalCentre getMedicalCentreToCreate() {
-        return medicalCentreToCreate;
-    }
-
-    public void setMedicalCentreToCreate(MedicalCentre medicalCentreToCreate) {
-        this.medicalCentreToCreate = medicalCentreToCreate;
-    }
-
-    public MedicalCentre getMedicalCentreToManage() {
-        return medicalCentreToManage;
-    }
-
-    public void setMedicalCentreToManage(MedicalCentre medicalCentreToManage) {
-        this.medicalCentreToManage = medicalCentreToManage;
-    }
-
-    public MedicalCentre getMedicalCentreToDelete() {
-        return medicalCentreToDelete;
-    }
-
-    public void setMedicalCentreToDelete(MedicalCentre medicalCentreToDelete) {
-        this.medicalCentreToDelete = medicalCentreToDelete;
-    }
-
-    public Boolean getIsEditable() {
-        return isEditable;
-    }
-
-    public void setIsEditable(Boolean isEditable) {
-        this.isEditable = isEditable;
-    }
-
 }
