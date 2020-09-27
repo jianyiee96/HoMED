bcbcba9987e4f3a15b7435e386df1219ee337b40
weekeat-tan <tanwk_97@hotmail.com>
2020-09-27 11:57:50 +0800

    Bulk create input stream done

--- HoMED/HoMED-war/src/java/jsf/managedBean/ServicemanAccountManagementManagedBean.java
+++ HoMED/HoMED-war/src/java/jsf/managedBean/ServicemanAccountManagementManagedBean.java
@@ -2,6 +2,11 @@ package jsf.managedBean;
 
 import ejb.session.stateless.ServicemanSessionBeanLocal;
 import entity.Serviceman;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.Serializable;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -49,17 +54,64 @@ public class ServicemanAccountManagementManagedBean implements Serializable {
         PrimeFaces.current().ajax().update("formAllServicemen:dataTableServicemen");
     }
 
-    public void handleFileUpload(FileUploadEvent event) {
-        FacesMessage msg = new FacesMessage("Successful", event.getFile().getFileName() + " is uploaded.");
-        FacesContext.getCurrentInstance().addMessage(null, msg);
-    }
-
     public void uploadCsv(FileUploadEvent event) {
         System.out.println("Uploading...");
         UploadedFile csv = event.getFile();
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        FacesMessage facesMessage = new FacesMessage();
 
         if (csv != null) {
             System.out.println("Uploaded file --> " + csv.getFileName() + " Size --> " + csv.getSize() + " ContentType --> " + csv.getContentType());
+
+            try {
+                InputStream csvInputStream = csv.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(csvInputStream));
+
+                String line = "";
+                Integer idx = 1;
+                while (reader.ready() && (line = reader.readLine()) != null) {
+                    String[] serviceman = line.split(",");
+                    String name = serviceman[0];
+                    String gender = serviceman[1];
+                    String phone = serviceman[2];
+                    String email = serviceman[3];
+                    String streetName = serviceman[4];
+                    String unitNumber = serviceman[5];
+                    String buildingName = serviceman[6];
+                    String country = serviceman[7];
+                    String postalCode = serviceman[8];
+                    String bloodType = serviceman[9];
+                    String rod = serviceman[10];
+
+                    System.out.println("Name = " + name + "; Gender = " + gender + "; Phone = " + phone + "; Email = " + email
+                            + "; Address (" + streetName + ", " + unitNumber + ", " + buildingName + ", " + country + ", " + postalCode
+                            + "); Blood Type = " + bloodType + "; ROD = " + rod);
+
+                    Serviceman newServiceman = new Serviceman();
+                    newServiceman.setName(name);
+
+                    if (gender.toUpperCase().equals("MALE") || gender.toUpperCase().equals("FEMALE")) {
+
+                    } else {
+                        facesMessage.setSeverity(FacesMessage.SEVERITY_ERROR);
+                        facesMessage.setSummary("Bulk Import");
+                        facesMessage.setDetail("[Line " + idx + " - " + name + "] has incorrect gender input (" + gender +")! Please modify it to MALE/FEMALE.");
+                        facesContext.addMessage("growl-message", facesMessage);
+                    }
+                }
+
+            } catch (IOException ex) {
+                ex.printStackTrace();
+                facesMessage.setSeverity(FacesMessage.SEVERITY_ERROR);
+                facesMessage.setSummary("Bulk Import");
+                facesMessage.setDetail("An unexpected error has occurred while creating servicemen in bulk. Please contact IT admins for assistance!");
+                facesContext.addMessage("growl-message", facesMessage);
+            }
+        } else {
+            facesMessage.setSeverity(FacesMessage.SEVERITY_ERROR);
+            facesMessage.setSummary("Bulk Import");
+            facesMessage.setDetail("An unexpected error has occurred while creating servicemen in bulk. Please contact IT admins for assistance!");
+            facesContext.addMessage("growl-message", facesMessage);
         }
     }
 
