19c78eb56b1cf9653bc70dc43c4489b0034dcc81
JianYiee <hew1521@hotmail.com>
2020-10-20 19:02:13 +0800

    update the update for booking slots

--- HoMED/HoMED-ejb/src/java/ejb/session/singleton/BookingExpiryManager.java
+++ HoMED/HoMED-ejb/src/java/ejb/session/singleton/BookingExpiryManager.java
@@ -51,7 +51,7 @@ public class BookingExpiryManager {
         c.set(Calendar.HOUR_OF_DAY, 0);
         c.set(Calendar.MINUTE, 0);
         c.set(Calendar.SECOND, 0);
-//        c.add(Calendar.DATE, 1);
+//        c.add(Calendar.DAT    E, 1);
         Date floorToday = c.getTime();
 
         System.out.println("- ExpiryManager: Marking bookings absent if start time is before " + df.format(floorToday));
