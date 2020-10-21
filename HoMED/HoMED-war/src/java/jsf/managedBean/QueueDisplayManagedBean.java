bc14244221d8759c9d75353b7661f762c4a2d50e
weekeat-tan <tanwk_97@hotmail.com>
2020-10-21 16:34:02 +0800

    Removed detect change check

--- HoMED/HoMED-war/src/java/jsf/managedBean/QueueDisplayManagedBean.java
+++ HoMED/HoMED-war/src/java/jsf/managedBean/QueueDisplayManagedBean.java
@@ -7,15 +7,12 @@ import entity.MedicalCentre;
 import entity.MedicalStaff;
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
-import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.inject.Named;
 import javax.faces.view.ViewScoped;
-import org.primefaces.PrimeFaces;
 import util.enumeration.ConsultationStatusEnum;
 
 @Named(value = "queueDisplayManagedBean")
@@ -31,12 +28,9 @@ public class QueueDisplayManagedBean implements Serializable {
     private List<Booking> currentBookings;
     private List<Booking> queueingBookings;
 
-    private HashSet<Long> changedBookingIdsHS;
-
     public QueueDisplayManagedBean() {
         currentBookings = new ArrayList<>();
         queueingBookings = new ArrayList<>();
-        changedBookingIdsHS = new HashSet<>();
     }
 
     @PostConstruct
@@ -52,41 +46,19 @@ public class QueueDisplayManagedBean implements Serializable {
 
     public void refreshDisplay() {
         List<Booking> newBookings = bookingSessionBeanLocal.retrieveQueueBookingsByMedicalCentre(currentMedicalCentre.getMedicalCentreId());
-        List<Booking> holderCurrentBookings = new ArrayList<>();
+
+        this.currentBookings.clear();
         this.queueingBookings.clear();
+
+        // To filter newly retrieved bookings into ONGOING and WAITING consultation bookings.
         newBookings.forEach(b -> {
             if (b.getConsultation().getConsultationStatusEnum() == ConsultationStatusEnum.ONGOING) {
-                holderCurrentBookings.add(b);
+                this.currentBookings.add(b);
             } else {
                 this.queueingBookings.add(b);
             }
         });
 
-        // DETECTING CHANGES HERE
-        this.changedBookingIdsHS.clear();
-        Boolean detectChange = this.currentBookings.size() != holderCurrentBookings.size();
-        if (!detectChange) {
-            holderCurrentBookings.stream()
-                    .map(b1 -> {
-                        Boolean notFound = this.currentBookings.stream().noneMatch(b2 -> b1.equals(b2));
-                        if (notFound) {
-                            changedBookingIdsHS.add(b1.getBookingId());
-                        }
-                        return notFound;
-                    })
-                    .reduce(false, (b1, b2) -> b1 || b2);
-        }
-        System.out.println("DETECT CHANGE: " + detectChange);
-        if (detectChange) {
-            // @WK - Can help me figure out a way to trigger CSS here (Blinking Queue Number)
-            this.currentBookings = holderCurrentBookings;
-            // Update has to be called here so that IDs can be generated first
-            PrimeFaces.current().ajax().update("formQueueDisplay:panel-all");
-//          :panel-current-bookings
-//          Component ID of specific card to blink: booking_#{booking.bookingId}
-            //Trigger JS
-//          PrimeFaces.current().executeScript("alert('peek-a-boo');");
-        }
     }
 
     public String formatQueueNumber(Long bookingId) {
@@ -116,9 +88,4 @@ public class QueueDisplayManagedBean implements Serializable {
     public List<Booking> getQueueingBookings() {
         return queueingBookings;
     }
-
-    public HashSet<Long> getChangedBookingIdsHS() {
-        return changedBookingIdsHS;
-    }
-
 }
