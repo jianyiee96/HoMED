ea7c02674c8057d90e066540fe3e8ba347448b6d
thumster <bryan.thum@hotmail.com>
2020-10-21 17:11:48 +0800

    added effect for new ongoing consultation

--- HoMED/HoMED-war/src/java/jsf/managedBean/QueueDisplayManagedBean.java
+++ HoMED/HoMED-war/src/java/jsf/managedBean/QueueDisplayManagedBean.java
@@ -7,12 +7,14 @@ import entity.MedicalCentre;
 import entity.MedicalStaff;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.context.FacesContext;
 import javax.inject.Named;
 import javax.faces.view.ViewScoped;
+import org.primefaces.PrimeFaces;
 import util.enumeration.ConsultationStatusEnum;
 
 @Named(value = "queueDisplayManagedBean")
@@ -28,9 +30,12 @@ public class QueueDisplayManagedBean implements Serializable {
     private List<Booking> currentBookings;
     private List<Booking> queueingBookings;
 
+    private HashSet<Long> changedBookingsHS;
+
     public QueueDisplayManagedBean() {
         currentBookings = new ArrayList<>();
         queueingBookings = new ArrayList<>();
+        changedBookingsHS = new HashSet<>();
     }
 
     @PostConstruct
@@ -46,19 +51,33 @@ public class QueueDisplayManagedBean implements Serializable {
 
     public void refreshDisplay() {
         List<Booking> newBookings = bookingSessionBeanLocal.retrieveQueueBookingsByMedicalCentre(currentMedicalCentre.getMedicalCentreId());
+        List<Booking> holderCurrentBookings = new ArrayList<>();
 
-        this.currentBookings.clear();
+//        this.currentBookings.clear();
         this.queueingBookings.clear();
 
         // To filter newly retrieved bookings into ONGOING and WAITING consultation bookings.
         newBookings.forEach(b -> {
             if (b.getConsultation().getConsultationStatusEnum() == ConsultationStatusEnum.ONGOING) {
-                this.currentBookings.add(b);
+                holderCurrentBookings.add(b);
             } else {
                 this.queueingBookings.add(b);
             }
         });
 
+        this.changedBookingsHS.clear();
+        System.out.println("DETECTNG CHANGES...");
+        holderCurrentBookings.stream()
+                .map(b1 -> {
+                    Boolean notFound = this.currentBookings.stream().noneMatch(b2 -> b1.equals(b2));
+                    if (notFound) {
+                        System.out.println("New booking found: " + b1);
+                        changedBookingsHS.add(b1.getBookingId());
+                    }
+                    return notFound;
+                })
+                .reduce(false, (b1, b2) -> b1 || b2);
+        this.currentBookings = holderCurrentBookings;
     }
 
     public String formatQueueNumber(Long bookingId) {
@@ -88,4 +107,9 @@ public class QueueDisplayManagedBean implements Serializable {
     public List<Booking> getQueueingBookings() {
         return queueingBookings;
     }
+
+    public HashSet<Long> getChangedBookingsHS() {
+        return changedBookingsHS;
+    }
+    
 }
