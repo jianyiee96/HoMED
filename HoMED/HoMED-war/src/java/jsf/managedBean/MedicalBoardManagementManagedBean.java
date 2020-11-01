b8a8628663fe57503e90255a0d5ed5232c0939e3
weekeat-tan <tanwk_97@hotmail.com>
2020-11-01 09:39:04 +0800

    WIP for medical board management

---
+++ HoMED/HoMED-war/src/java/jsf/managedBean/MedicalBoardManagementManagedBean.java
@@ -0,0 +1,43 @@
+/*
+ * Project Title: Home Team Medical Board
+ * Project Application: HoMED-war
+ */
+package jsf.managedBean;
+
+import ejb.session.stateless.SlotSessionBeanLocal;
+import entity.MedicalBoardSlot;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.inject.Named;
+import javax.faces.view.ViewScoped;
+
+@Named(value = "medicalBoardManagementManagedBean")
+@ViewScoped
+public class MedicalBoardManagementManagedBean implements Serializable {
+
+    @EJB(name = "SlotSessionBeanLocal")
+    private SlotSessionBeanLocal slotSessionBeanLocal;
+
+    private List<MedicalBoardSlot> medicalBoardSlots;
+
+    public MedicalBoardManagementManagedBean() {
+        this.medicalBoardSlots = new ArrayList<>();
+    }
+
+    @PostConstruct
+    public void postConstruct() {
+        this.medicalBoardSlots = slotSessionBeanLocal.retrieveMedicalBoardSlots();
+    }
+
+    public List<MedicalBoardSlot> getMedicalBoardSlots() {
+        return medicalBoardSlots;
+    }
+
+    public void setMedicalBoardSlots(List<MedicalBoardSlot> medicalBoardSlots) {
+        this.medicalBoardSlots = medicalBoardSlots;
+    }
+
+}
